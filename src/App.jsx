56ae9c36ce9dc804289876a9ca51a405191faccf
Shreekant <shreekant4062@gmail.com>
2025-04-14 14:45:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen ">
+      <h1 className="text-3xl font-bold text-red-300 font-mont"> 
+        Bun vite app with react
+      </h1>
+    </div>
+  );
+};
+
+export default App;
