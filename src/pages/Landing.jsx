bea2662e8765e4545ceaa1f0a9535ed440543585
Shreekant <shreekant4062@gmail.com>
2025-04-14 17:25:45 +0530

    hero section added

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Section1 from "../components/Landing/section1";
+
+const Landing = () => {
+  return (
+    <div className="h-screen  bg-[#FEF7F2] dark:bg-gray-900 text-gray-700 dark:text-gray-200">
+      
+      <Section1 />
+    </div>
+  );
+};
+
+export default Landing;
